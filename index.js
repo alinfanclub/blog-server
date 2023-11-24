8c65e4c9b439b7856ca3164cede8368f69966ab8
alinfanclub <e759ksh@naver.com>
2023-11-24 12:05:46 +0900

    port setting

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import express from "express";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import chalk from "chalk";
 import mongoose from "mongoose";
 import userRoute from "./src/routes/userRouter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./src/routes/postRouter.js";
+import detectPort from "detect-port";
 
-config();
+dotenv.config();
 
 const app = express();
 
 const MONGO_URI = `mongodb+srv://e759ksh:${process.env.MONGO_PASSWORD}@cluster0.x86fylx.mongodb.net/?retryWrites=true&w=majority`;
+
 const server = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-
+    try {
+      await mongoose.connect(MONGO_URI);
+    } catch (error) {
+      console.log(chalk.bgRedBright("Failed to connect to MongoDB"));
+    }
     mongoose.set("debug", true);
     console.log(chalk.bgRedBright("Connected to MongoDB"));
 
@@ -31,12 +36,17 @@ const server = async () => {
 
     app.use("/user", userRoute);
     app.use("/post", postRoute);
-    const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, async () => {
-      console.log(chalk.bgGreen(`Server is running on port ${PORT}`));
+    app.set("port", process.env.PORT || 3000);
+
+    app.listen(app.get("port"), async () => {
+      console.log(
+        chalk.bgGreen(`Server is running on port ${app.get("port")}`)
+      );
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 server();
