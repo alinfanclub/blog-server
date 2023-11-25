5f7be20d27bd25470473f17a7989cb708f4d4245
e759ksh <e759ksh@naver.com>
2023-11-25 22:47:16 +0900

    fsdfad

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -144,7 +144,7 @@ userRoute.post("/login", async (req, res) => {
     res.cookie("jwt", token, {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       sameSite: "Lax",
-      httpOnly: true,
+      httpOnly: false,
       secure: true,
     });
 
