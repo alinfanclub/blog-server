383e399669c0955dc39ab6a726e799945f8ea361
e759ksh <e759ksh@naver.com>
2023-11-25 15:59:54 +0900

    path 추가

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -144,8 +144,9 @@ userRoute.post("/login", async (req, res) => {
     res.cookie("jwt", token, {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       sameSite: "Lax",
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
+      path: "/",
     });
 
     return res.status(200).json({
