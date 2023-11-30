eac7d27edc097cb9fafc6c5f8351a6718f042715
alinfanclub <e759ksh@naver.com>
2023-11-30 13:52:41 +0900

    redirect 수정

--- index.js
+++ index.js
@@ -41,10 +41,7 @@ const server = async () => {
     app.use("/admin", function (req, res, next) {
       const jwt = req.cookies.jwt;
       if (!jwt) {
-        return res
-          .status(401)
-          .send({ error: "로그인이 필요합니다." })
-          .redirect("/");
+        return res.redirect("/");
       }
       next();
     });
